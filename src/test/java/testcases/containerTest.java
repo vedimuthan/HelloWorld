c7e445925e54c8d39694d64bffa38a0dc714fecb
vedimuthan <vedimuthan1987@gmail.com>
2020-02-21 16:40:44 -0500

    Added Test cases

---
+++ src/test/java/testcases/containerTest.java
@@ -0,0 +1,67 @@
+package test.java.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class containerTest {
+
+	 //-----------------------------------Global Variables-----------------------------------
+    //Declare a Webdriver variable
+    public WebDriver driver;
+ 
+    //Declare a test URL variable
+    public String testURL = "http://192.168.99.100:8080/clientapp/index.html";
+ 
+    //-----------------------------------Test Setup-----------------------------------
+    @BeforeMethod
+    public void setupTest (){
+        //Create a new ChromeDriver
+    	ChromeOptions options = new ChromeOptions();
+    	options.addArguments("–no-sandbox");
+		options.addArguments("single-process");
+		options.addArguments("start-maximized");         // open Browser in maximized mode
+		options.addArguments("disable-infobars");        // disabling infobars
+    	options.addArguments("--headless");
+    	options.addArguments("--disable-gpu");
+    	options.addArguments("'window-size=1024,768'");  
+        options.addArguments("--disable-features=VizDisplayCompositor");
+        options.addArguments("--incognito");
+        options.addArguments("enable-automation");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--dns-prefetch-disable");
+
+    	
+    	//options.setExperimentalOption(useAutomationExtension, false);
+        driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+        //Go to www.swtestacademy.com
+        driver.navigate().to(testURL);
+    }
+ 
+    //-----------------------------------Tests-----------------------------------
+    @Test
+    public void firstTest () {
+        //Get page title
+        String title = driver.getTitle();
+ 
+        //Print page's title
+        System.out.println("Page Title: " + title);
+ 
+        //Assertion
+        Assert.assertEquals(title, "Hello World", "Title assertion is failed!");
+    }
+ 
+    //-----------------------------------Test TearDown-----------------------------------
+    @AfterMethod
+    public void teardownTest (){
+        //Close browser and end the session
+        driver.quit();
+    }
+
+}
